743e995a456848f5d7caee921c5b9aef006ccf9b
Slada Team <slada.team@gmail.com>
2020-04-20 01:37:19 +0700

    update config

--- quasar.conf.js
+++ quasar.conf.js
@@ -107,16 +107,16 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'slada.id Fresh Everyday',
+        name: 'Slada.id Fresh Everyday',
         
-        short_name: 'slada.id Fresh Everyday',
+        short_name: 'Slada.id Fresh Everyday',
         description: 'Shop Vegetable Make Easy',
         display: 'standalone',
         orientation: 'portrait',
         
         background_color: '#ffffff',
         
-        theme_color: '#027be3',
+        theme_color: '#439534',
         icons: [
           {
             src: 'statics/icons/icon-128x128.png',
